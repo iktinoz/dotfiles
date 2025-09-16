c8bc196dfa4907f4086dd7ae9aeeb1ffe29a6214
Patrick Bruns <patrick.bruns@offis.de>
2025-09-16 09:32:29 +0200

    add pipx setting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
    alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 end
+
+# Created by `pipx` on 2025-09-09 07:58:04
+set PATH $PATH /home/pbruns/.local/bin
